5b60d5a530d311fa76dd7024f161a5dce065ea4a
Crayyyyyy <robert.plavka@gmail.com>
2024-08-17 22:22:40 +0200

    Last portfolio version

--- src/components/Trays/ResumeTray.vue
+++ src/components/Trays/ResumeTray.vue
@@ -1,10 +1,71 @@
 <template>
-    <div class="tray resume">
-        <!-- <h1>Grab my résumé!</h1>
+    <div id="resume" class="tray">
+        <h1>Grab my résumé!</h1>
         <p>Take a closer look at history of my studies and work experience.</p>
-        <button>
+        <button id="download-button">
           <h2 class="download-text">Download CV</h2>
           <img src="../../assets/icons/download.png" alt="">
-        </button> -->
+        </button>
       </div>
 </template>
+
+<style>
+
+#resume
+{
+    /* SIZE */
+    width: 50vw;
+    padding: var(--padding_m);
+
+    /* FLEX */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: left;
+}
+
+#download-button
+{
+    /* COLOR */
+    background: var(--white_color);
+
+    /* SIZE */
+    width: 50%;
+    height: 60%;
+
+    /* FLEX */
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    align-self: center;
+}
+
+#download-button:hover
+{
+    cursor: pointer;
+}
+
+#download-button img
+{
+    width: 10%;
+}
+
+#resume h1
+{
+    /* TEXT */
+    font-size: var(--font_xl);
+}
+
+#resume h2
+{
+    /* TEXT */
+    color: var(--back_color);
+    font-size: var(--font_l);
+
+    /* OFFSET */
+    padding-left: var(--padding_m);
+    padding-right: var(--padding_m);
+}
+
+</style>
--- src/components/Trays/SkillsTray.vue
+++ src/components/Trays/SkillsTray.vue
@@ -1,15 +1,14 @@
 <template>
     <div id="skills" class="tray">
         <h1 class="top">My skills ?</h1>
-        <h2 class="under-top">School related</h2>
         <div class="skill-icons">
-            <img src="../../assets/icons/bash.svg" alt="">
-            <img src="../../assets/icons/html.svg" alt="">
-            <img src="../../assets/icons/css.svg" alt="">
-            <img src="../../assets/icons/javascript.svg" alt="">
-            <img src="../../assets/icons/python.svg" alt="">
-            <img src="../../assets/icons/java.svg" alt="">
-            <img src="../../assets/icons/github.svg" alt="">
+            <div class="icon"><img src="../../assets/icons/html.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/css.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/javascript.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/bash.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/python.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/java.svg" alt=""></div>
+            <div class="icon"><img src="../../assets/icons/github.svg" alt=""></div>
         </div>
     </div>
 </template>
@@ -18,7 +17,33 @@
 
 #skills
 {
-    @apply row-span-2 col-span-2;
+    @apply flex flex-col row-span-2 col-span-2;
 }
+
+.skill-icons
+{
+    @apply h-full;
+    /* FLEX */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.icon
+{
+    /* FLEX */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    min-width: 30%;
+}
+
+.icon img
+{
+    @apply w-2/3;
+}
+
 
 </style>
